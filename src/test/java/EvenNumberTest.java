e300167d013a95169b5005854f822bedf1292dc5
Alexandr <jwchaganav@gmail.com>
2024-02-17 08:49:05 +0400

    coverage

--- src/test/java/EvenNumberTest.java
+++ src/test/java/EvenNumberTest.java
@@ -7,21 +7,34 @@ import org.junit.jupiter.api.*;
 import static org.assertj.core.api.Assertions.*;
 
 
-
+/**
+ * Метод проверяет, является ли целое число записанное в переменную n,
+ * чётным (true) либо нечётным (false).
+ */
 public class EvenNumberTest {
 
     Calculation calc = new Calculation();
+
+    /**
+     * проверка на четное
+     */
     @Test
-    public void eventNumberTest(){
+    public void evenNumberTest(){
 
         assertThat(calc.evenOddNumber(3)).isEqualTo(false);
     }
 
+    /**
+     * проверка на четное
+     */
     @Test
     public void nullNumberTest(){
         assertThat(calc.evenOddNumber(0)).isEqualTo(true);
     }
 
+    /**
+     * проверка на нечетное
+     */
     @Test
     public void oddNumber(){
         assertThat(calc.evenOddNumber(6)).isEqualTo(true);
--- src/test/java/IntervalTest.java
+++ src/test/java/IntervalTest.java
@@ -3,18 +3,32 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
+/**
+ * метод, который проверяет, попадает ли переданное число в интервал (25;100)
+ * и возвращает true, если попадает и false - если нет
+ */
 public class IntervalTest {
     Calculation calc = new Calculation();
+
+    /**
+     * проверка, если меньше
+     */
     @Test
     public void intervalLessTest(){
         assertThat(calc.interval(24)).isEqualTo(false);
     }
 
+    /**
+     * проверка, если больше
+     */
     @Test
     public void intervalMoreTest(){
         assertThat(calc.interval(101)).isEqualTo(false);
     }
 
+    /**
+     * проверка, если входит в интервал
+     */
     @Test
     public void intervalEqualTest(){
         assertThat(calc.interval(99)).isEqualTo(true);
